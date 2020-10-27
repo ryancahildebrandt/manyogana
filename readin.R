55a4d1b4842d03ef4c0e593a6a011d198d56ef89
Ryan <ryancahildebrandt@gmail.com>
2020-10-27 16:34:02 -0400

    10.27

--- readin.R
+++ readin.R
@@ -1,4 +1,6 @@
 # Doc Setup----
+renv::activate()
+renv::restore()
 library(rvest)
 library(reshape2)
 library(zipangu)
@@ -18,17 +20,89 @@ renv::snapshot()
 # parse into tokens
 # output manyogana transcription
 
-
-# In the poem, the sounds mo (母, 毛) and shi (之, 思) are written with multiple, different characters. While all particles and most words are represented phonetically (e.g., 多太 tada, 安佐 asa), the words ji (路), umi (海) and funekaji (船梶) are rendered semantically.
-# In some cases, specific syllables in particular words are consistently represented by specific characters. This usage is known as Jōdai Tokushu Kanazukai. This usage has led historical linguists to conclude that certain disparate sounds in Old Japanese, consistently represented by differing sets of man'yōgana characters, may have merged since then.
-# Shakuon kana (借音仮名) are based on a Sino-Japanese on'yomi reading, in which one character represents either one mora or two morae.[6]
-# Shakkun kana (借訓仮名) are based on a native kun'yomi reading, one to three characters represent one to three morae.[6]
-# The vocalic distinction (1 versus 2) is very important linguistically, but for literary purposes (or while reading for pleasure) it can be ignored – as it often is in Japanese works, hence no differentiation is usually made in the hiragana transcription. 
-
 #full text
 #http://vsarpj.orinst.ox.ac.uk/corpus/ojcorpus.html#MYS
 #https://www.aclweb.org/anthology/W16-4006/
 
+#Functions----
+get_script <- function(x) {
+  x <- strsplit(x, split="") %>% unlist(.)
+  script <- case_when(
+    grepl("[ぁ-ん]",x) ~ "hiragana",
+    grepl("[ァ-ン]",x) ~ "katakana",
+    grepl("[0-9]",x) ~ "numeric",
+    grepl("[一-龯]",x) ~ "kanji",
+    grepl("[A-Za-z]",x) ~ "romaji",
+    TRUE ~ x)
+  return(script)
+}
+
+part_conj_myg <- function(x) {
+  get_pc_myg <- function(x) {
+    myg<-ifelse(get_script(x) %>% sapply(., unique)=="hiragana",
+                wiki_hiragana$漢字[wiki_hiragana$かな == x],
+                "_")
+    return(myg)
+  }
+  l1<-unlist(strsplit(x, split=""))
+  myg<-paste(sapply(l1, get_pc_myg), collapse="")
+  return(myg)
+}
+
+get_myg_options<-function(x){
+  myg<-wiki_manyogana$漢字[wiki_manyogana$かな == x]
+  return(myg)
+}
+
+kana_word_myg <- function (xin) {
+  xin_h<-str_conv_hirakana(xin, to="hiragana")
+  ifelse(get_script(xin) %>% unique(.) =="katakana" | get_script(xin) %>% unique(.) =="hiragana",
+         {l1<-unlist(strsplit(xin_h, split=""))
+         mt<-mapply(get_myg_options, l1)
+         
+         get_cos_myg <- function(mt_i) {
+           mlist<-sapply(X = mt_i, FUN = Cosine, y = xin, tvectors = w2v_jp) 
+           maxcos<-names(mlist[mlist == max(mlist)])
+           return(maxcos)
+         }
+         myg<-paste(sapply(FUN=get_cos_myg, mt), collapse="")
+         },
+         myg<-part_conj_myg(xin))
+  return(myg)
+}
+
+mxd_myg<- function(xin) {
+  xvec<-unlist(strsplit(xin, split=""))
+  myg<-paste(case_when(
+    get_script(xvec) == "kanji" ~ xvec,
+    get_script(xvec) == "hiragana"~ sapply(xvec, part_conj_myg),
+    TRUE ~ xvec), collapse="")
+  return(myg)
+}
+
+num_suuji<-function (xin=999999999999999){
+  options("scipen"=999)
+  
+  xinstr<-str_pad(as.character(xin) , width=17, side="left", pad="0")
+  xinstr_vec<-unlist(strsplit(xinstr, split=""))
+  xinstr_kanji<-sapply(xinstr_vec, function(i) suuji_df$suuji[i==suuji_df$num], USE.NAMES = FALSE)
+  
+  suuji_pat<-c("京","千","百","十","兆","千","百","十","億","千","百","十","万","千","百","十","一")
+  suuji_1<-suuji_pat %>%
+    rbind(xinstr_kanji %>% 
+            paste(., collapse="") %>% 
+            strsplit(.,split="") %>% 
+            unlist(.),.) %>%
+    c(.) %>%
+    paste(., collapse="") %>% 
+    stri_sub(., 0,-2) %>%
+    gsub("([一二三四五六七八九]十)○([^○])","\\1\\2",., perl=TRUE) %>% 
+    gsub("(○.)*", "", .) %>% 
+    gsub("○", "", .) %>% 
+    gsub("一(百|十)", "\\1", .)
+  
+  return(list(suuji_1))
+}
 
 
 #Wiki scrape----
@@ -40,7 +114,9 @@ wiki_shakuon <- wikiurl[2] %>% data.frame(.)
 wiki_shakkun <- wikiurl[3] %>% data.frame(.)
 wiki_katakana <- wikiurl[5] %>% data.frame(.)
 
-#Wiki cleaning----
+
+
+#DF cleaning----
 wiki_hiragana <- wikiurl[6] %>% 
   data.frame(.) %>%
   melt(., id = "Var.1") %>% 
@@ -61,15 +137,11 @@ wiki_hiragana <- wikiurl[6] %>%
   rename(., "漢字" = value) %>% 
   select(., "かな", "漢字") %>% 
   rbind(., data.frame(
-    かな=c("が","で","ば","ず","べ"),
-    漢字=c("賀","傅","婆","受","倍") # pulled from manyoshu
+    かな=c("が","で","ば","ず","べ","ぎ","ぐ","げ","ご","ざ","じ","ぜ","ぞ","だ","ぢ","っ","づ","ど","ぱ","び","ぴ","ぶ","ぷ","ぺ","ぼ","ぽ","ゃ","ゅ","ょ"),
+    漢字=c("賀","傅","婆","受","倍","芸","具","宜","其","射","士","是","曽","太","知","都","豆","杼","波","毘","毘","夫","布","閇","煩","富","夜","由","余") # pulled from manyoshu
   )) %>% 
   distinct(.)
   
-kana_vec<-strsplit("あいうえおかがきぎくぐけげこごさざしじすずせぜそぞただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽまみむめもやゆよらりるれろわをん", split="")
-#################intersect(kana_vec, as.character(wiki_hiragana$かな))
-
-
 wiki_manyogana <- wikiurl[4] %>% 
   data.frame(.) %>%
   mutate(., X1 = ifelse(.$X1 %in% "", "_", .$X1)) %>% 
@@ -103,143 +175,46 @@ wiki_manyogana <- wikiurl[4] %>%
   transmute(., 
             かな = .$かな,
             漢字 = sapply(.$漢字, unique))
-  
 
-#NLP---- 
-get_script <- function(x) {
-  x <- strsplit(x, split="") %>% unlist(.)
-  script <- case_when(
-    grepl("[ぁ-ん]",x) ~ "hiragana",
-    grepl("[ァ-ン]",x) ~ "katakana",
-    grepl("[０-９]",x) ~ "numeric",
-    grepl("[一-龯]",x) ~ "kanji",
-    grepl("[A-Za-z]",x) ~ "romaji",
-    TRUE ~ x)
-  return(script)
-}
+suuji_df<-data.frame(
+  num = c("0","1","2","3","4","5","6","7","8","9","10","100","1000","10000","100000000","1000000000000","10000000000000000"),
+  suuji = c("○","一","二","三","四","五","六","七","八","九","十","百","千","万","億","兆","京"), 
+  stringsAsFactors = FALSE) 
 
+#NLP setup---- 
 load("./data/w2v_jp.RData")
-exin <- c("私は韓国へ行くつもりです.")
-
-
-#kanji_myg
-# kanji words: leave meaning as much as possible
-#  func unnecessary
-
-
-part_conj_myg <- function(x) {
-  get_pc_myg <- function(x) {
-    myg<-ifelse(get_script(x) %>% sapply(., unique)=="hiragana",
-           wiki_hiragana$漢字[wiki_hiragana$かな == x],
-           "_")
-    return(myg)
-  }
-  l1<-unlist(strsplit(x, split=""))
-  myg<-paste(sapply(l1, get_pc_myg), collapse="")
-  return(myg)
-}
-
-get_myg_options<-function(x){
-  myg<-wiki_manyogana$漢字[wiki_manyogana$かな == x]
-  return(myg)
-}
-
-kana_word_myg <- function (xin) {
-  ifelse(xin!="_",
-         {l1<-unlist(strsplit(xin, split=""))
-         mt<-mapply(get_myg_options, l1)
-         
-         get_cos_myg <- function(mt_i) {
-           mlist<-sapply(X = mt_i, FUN = Cosine, y = xin, tvectors = w2v_jp) 
-           maxcos<-names(mlist[mlist == max(mlist)])
-           return(maxcos)
-         }
-         myg<-paste(sapply(FUN=get_cos_myg, mt), collapse="")
-         },
-         myg<-"_")
-   
-    return(myg)
-}
-
-mxd_myg<- function(xin) {
-  
-}
-xin<-"食べる"
-xvec<-unlist(strsplit(xin, split=""))
-get_script(xvec)
-case_when(
-  get_script(xvec) == "kanji" ~ xvec,
-  get_script(xvec) == "hiragana"~ sapply(xvec, part_conj_myg),
-  TRUE ~ xvec)
-
-mxd_myg("食べる")
-str_conv_normalize(xin)
-
-
-
-#kana_myg
-#romaji_myg
-#other
-
-
-
-
-
-
 udmodel_file <- udpipe_download_model(language= "japanese")
 udmodel_jp <- udpipe_load_model(file = udmodel_file$file_model)
+options("scipen"=999)
 
-ud_df <- udpipe_annotate(udmodel_jp, x = exin) %>% 
-  as.data.frame(.) %>% 
-  mutate(., script = sapply(.$token, get_script)) %>% 
-  mutate(., kana = ifelse(.$upos == "NOUN" & sapply(.$script, unique) == "hiragana", .$token, "_")) %>% 
-  mutate(., myg =  case_when(
-    sapply(.$script, unique) == "kanji" ~ .$token,
-    .$upos == "ADP" ~ paste(sapply(.$token, part_conj_myg)),
-    .$upos == "VERB" & sapply(.$script, unique) == "hiragana" ~ paste(sapply(.$token, part_conj_myg)),
-    .$upos == "AUX" & sapply(.$script, unique) == "hiragana" ~ paste(sapply(.$token, part_conj_myg)),
-    .$upos == "NOUN" & sapply(.$script, unique) =="hiragana" ~ sapply(ud_df$kana, kana_word_myg)))
+#Transliteration----
+  
+transliterate_it<-function(xin="使ってみて下さいね！"){
+  options(warn=-1)
+  ud_df <- udpipe_annotate(udmodel_jp, x = xin) %>% 
+    as.data.frame(.) %>% 
+    mutate(., script = sapply(.$token, get_script)) %>% 
+    mutate(., kana = case_when(
+      .$upos == "NOUN" & sapply(.$script, unique) == "hiragana" ~ .$token,
+      TRUE ~ sapply(.$token, str_conv_hirakana, to="hiragana"))) %>% 
+    mutate(., myg =  case_when(
+      sapply(.$script, unique) == "kanji" ~ .$token,
+      sapply(.$script, unique) == "numeric" ~ paste(sapply(.$token, num_suuji)),
+      .$upos == "ADP" ~ paste(sapply(.$token, part_conj_myg)),
+      .$upos == "VERB" & sapply(.$script, unique) == "hiragana" ~ paste(sapply(.$token, part_conj_myg)),
+      (.$upos == "AUX" | .$upos == "SCONJ") & sapply(.$script, unique) == "hiragana" ~ paste(sapply(.$token, part_conj_myg)),
+      sapply(sapply(.$script, unique), length) >= 2 ~ paste(sapply(.$kana, mxd_myg)),
+      .$upos == "PUNCT" | .$upos == "SYM"  ~ .$token,
+      TRUE ~ "MYG")) %>% 
+    mutate(., myg = ifelse(.$myg=="MYG", sapply(.$token[.$myg=="MYG"],kana_word_myg), .$myg)) 
+  xout <- paste(ud_df$myg, collapse="")
+  return(list(xin,xout))
+}
+transliterate_it("こん日は!")
 
-
-
-
-sapply(ud_df$script, unique)
-
-
-
-
-
-
-trans_df <- data.frame(
-  input = strsplit(exin, split = ""),
-  script = get_script(exin),
-  pos = rep(ud_df$upos,sapply(ud_df$script,length)),
-  stringsAsFactors = FALSE) %>% 
-  set_colnames(., c("input","script", "pos")) %>% 
-  mutate(.,
-         transliteration = case_when(
-           .$script == "kanji" ~ .$input,
-           .$pos == "ADP" ~ sapply(trans_df$input, part_conj_myg),
-           .$pos == "VERB" & .$script == "hiragana"  ~ sapply(trans_df$input, part_conj_myg),
-           .$pos == "AUX" & .$script == "hiragana"  ~ sapply(trans_df$input, part_conj_myg),
-           .$pos == "NOUN" & .$script == "hiragana"  ~ "blank",
-           TRUE ~ .$input
-         ))
-
-paste(.$input[.$pos == "NOUN" & .$script == "hiragana"], collapse="") %>% kana_word_myg(.)
+#works better if any possible words are in form found in dict, probably as much kanji as possible
 
 
-
-
-
-
-
-
-
-
-
-exout <- paste(trans_df$input, collapse="")
-exout
 #JMDict----
 
 # 
